fa5875698c0e578397b2e01269ff32cc6401c344
aditehh <aditya.aryal888@gmail.com>
2025-11-22 00:58:52 +0545

    thisthat

---
+++ third.js
@@ -0,0 +1,9 @@
+var myUser = {
+    _id: "1243",
+    name: "a",
+    email: "h@h.com",
+    isactive: false
+};
+myUser.email = "h@gmail.com";
+// myUser._id = "klafsdj"
+console.log(myUser.email);
---
+++ third.ts
@@ -0,0 +1,20 @@
+type User = {
+    readonly _id: string; //readonly means that, that value is not changable, the program is not allowed to modify it 
+    name: string;
+    email: string;
+    isactive: boolean;
+    creditcarddetails?: number; //when we give qn mark then that value holder becomes optional so when we define the actual value in the object section down it doesnt give us any swiggly lines as it knows its marked as options
+}
+
+let myUser: User = {
+    _id: "1243",
+    name: "a",
+    email: "h@h.com",
+    isactive: false,
+    // creditcarddetails: 23324543
+}
+
+type cardnumber
+
+myUser.email = "h@gmail.com"
+console.log(myUser.email)
