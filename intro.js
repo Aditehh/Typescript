66f383a42b58cc52ec276b96d5fae94e6d7c8c81
aditehh <aditya.aryal888@gmail.com>
2025-11-21 15:57:09 +0545

    this

--- intro.js
+++ intro.js
@@ -1,2 +1,4 @@
 var messi = "this is the best players in the world";
 console.log(messi);
+var greetings = "hello";
+console.log(greetings);
--- intro.ts
+++ intro.ts
@@ -1,2 +1,7 @@
 let messi: string = "this is the best players in the world"
 console.log(messi)
+
+let greetings: string = "hello"
+console.log(greetings)
+
+export {}
