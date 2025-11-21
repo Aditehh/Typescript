2c7c3d5e533b0585b45f0685ccf38a1a5b1944f4
aditehh <aditya.aryal888@gmail.com>
2025-11-21 22:58:51 +0545

    thisthatthisandthat

--- 1.js
+++ 1.js
@@ -14,3 +14,17 @@ var loginUser = function (name, email, ispaid) {
 addtwo(5);
 getUpper("hitesh");
 loginUser("a", "a@a.com");
+// function getvalue(myval: number): {
+//     if(myval > 5) {
+//         return true;
+//     }
+//     return "200 OK"
+// }
+var gethello = function (s) {
+    return "";
+};
+var heros = ["thor", "spiderman", "ironman"];
+// const heros = [1,2,3]
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
--- 1.ts
+++ 1.ts
@@ -1,4 +1,4 @@
-function addtwo(num: number) {
+function addtwo(num: number): number {
 
     return num + 2
 
@@ -12,10 +12,31 @@ function signup(name: string, email: string, password: boolean) {
 
 }
 
-let loginUser = (name: string, email: string, ispaid: boolean=false) => {}
+let loginUser = (name: string, email: string, ispaid: boolean = false) => { }
 
 addtwo(5)
 getUpper("hitesh")
 loginUser("a", "a@a.com")
+
+
+// function getvalue(myval: number): {
+//     if(myval > 5) {
+//         return true;
+//     }
+//     return "200 OK"
+// }
+
+const gethello = (s: string): string => {
+    return ""
+}
+
+const heros = ["thor", "spiderman", "ironman"]
+// const heros = [1,2,3]
+
+heros.map((hero):string => {
+    return `hero is ${hero}`
+}) 
+
+
 
 export { }
