6349a7940c94728d870013e0552819a8e2164a92
aditehh <aditya.aryal888@gmail.com>
2025-11-22 14:42:54 +0545

    arrays.ts

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var superheros = [];
+var heropower = [];
+var allusers = [];
+superheros.push("spiderman");
+heropower.push(23);
+allusers.push({ name: "hitesh", isactive: false });
+console.log(superheros);
+console.log(heropower);
+console.log(allusers);
---
+++ arrays.ts
@@ -0,0 +1,25 @@
+const superheros: string[] = []
+const heropower: number[] = []
+
+
+
+type Users = {
+    name: string;
+    isactive: boolean
+}
+
+const allusers: Users[] = []
+
+
+const mlmodels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+
+superheros.push("spiderman")
+heropower.push(23)
+allusers.push({ name: "hitesh", isactive: false })
+console.log(superheros)
+console.log(heropower)
+console.log(allusers)
