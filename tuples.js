1c80d2c2b43ddd622b25bbe4dfd65256703fd1cf
aditehh <aditya.aryal888@gmail.com>
2025-11-24 14:03:19 +0545

    this

---
+++ tuples.js
@@ -0,0 +1,4 @@
+// tuple is a specialized array type that allows you to express an array with a fixed number of elements where each element has a specific order
+var person;
+person = ["aa", 19, false];
+console.log(person[0]);
---
+++ tuples.ts
@@ -0,0 +1,10 @@
+// tuple is a specialized array type that allows you to express an array with a fixed number of elements where each element has a specific order
+
+
+
+let person: [string, number, boolean]
+
+person = ["aa", 19, false]
+
+console.log(person[0])
+
